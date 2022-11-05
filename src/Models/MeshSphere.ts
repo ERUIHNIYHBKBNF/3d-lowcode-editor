fd930df7a16f66880ac588aab69de2bea7fd9605
chino <839203521@qq.com>
2022-11-05 16:13:16 +0800

    refactor: Detail adjustment

--- src/Models/MeshSphere.ts
+++ src/Models/MeshSphere.ts
@@ -1,4 +1,6 @@
+/* 创建一个没啥用的网格球 */
 import { SphereGeometry, MeshBasicMaterial, Mesh } from "three";
+
 export function MeshSphere(): Mesh & { animation?: () => void } {
   const geometry = new SphereGeometry(1, 64, 64);
   const material = new MeshBasicMaterial({
